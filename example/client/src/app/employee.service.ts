2e8ecfccef542864579f2254b3dfb1ba5fbf9ea2
sangramPeerI <sangram.konde@peerislands.io>
2023-02-02 00:06:10 -0500

    removed commented code

--- example/client/src/app/employee.service.ts
+++ example/client/src/app/employee.service.ts
@@ -5,31 +5,20 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, Subject, tap } from 'rxjs';
 import { Employee } from './employee';
 import { EnvVariablesServiceService } from './env-variables-service.service';
-// import { * }  from '@dotenv';
 
-// Load environment variables from the .env file, where the ATLAS_URI is configured
-// dotenv.config();
-// console.log(process)
-// const ATLAS_URI = process.env
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  /*private url = 'http://partner-demo-eb-meanstack-dev.us-east-1.elasticbeanstalk.com:5200'; */
-//9fe9cd91d3863eaa.elb.us-east-1.amazonaws.com
+
   private url:any;
 
-  // private url :any;
   private employees$: Subject<Employee[]> = new Subject();
 
   constructor(private httpClient: HttpClient,private service: EnvVariablesServiceService) { 
     this.url = this.service.ENV.ATLAS_URI
     console.log("ENV varible ", this.url)
   }
-
-  // private employees$: Subject<Employee[]> = new Subject();
-
-  // constructor(private httpClient: HttpClient) { }
 
   private refreshEmployees() {
     this.httpClient.get<Employee[]>(`${this.url}/employees`)
